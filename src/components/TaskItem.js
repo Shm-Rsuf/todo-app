74cb8d3e93ae849c30c7e435af78e4de491c9aa5
shm_usuf <shmusuf@gmail.com>
2022-12-01 20:49:04 +0600

    todo app done

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,9 +1,10 @@
 import { useContext } from "react";
 import { FiEdit, FiTrash } from "react-icons/fi";
-import { DeleteHandlerContext } from "../App";
+import { DeleteHandlerContext, EditHandlerContext } from "../App";
 
-const TaskItem = ({ task }) => {
+const TaskItem = ({ task, handleEditSubmitter, editedText, setEditedText }) => {
   const handleDelete = useContext(DeleteHandlerContext);
+  const handleEdit = useContext(EditHandlerContext);
 
   return (
     <div className="task-item flex justify-between items-center bg-gray-800 p-4 rounded hover:bg-gradient-to-r hover:from-teal-800 hover: to-gray-700 duration-300 group">
@@ -11,12 +12,26 @@ const TaskItem = ({ task }) => {
         <span>
           <input className="accent-teal-500" type="checkbox" />
         </span>
-        <p className="group-hover:text-teal-500">{task.text}</p>
+        {task.isEditable && (
+          <form onSubmit={(e) => handleEditSubmitter(e, task.id)}>
+            <input
+              className="bg-transparent outline-none border-b-2 border-gray-600 pb-1 focus:border-teal-500"
+              type="text"
+              required
+              value={editedText}
+              onChange={(e) => setEditedText(e.target.value)}
+            />
+          </form>
+        )}
+
+        {!task.isEditable && (
+          <p className="group-hover:text-teal-500">{task.text}</p>
+        )}
       </div>
       <div className="task-item-right flex gap-2 items-center">
-        <span>
+        <button onClick={() => handleEdit(task.id)}>
           <FiEdit className="text-gray-500 hover:text-teal-500 cursor-pointer duration-300" />
-        </span>
+        </button>
         <button onClick={() => handleDelete(task.id)}>
           <FiTrash className="text-gray-500 hover:text-red-500 cursor-pointer duration-300" />
         </button>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,28 @@
 import TaskItem from "./TaskItem";
 
-const TaskList = ({ tasks }) => {
+const TaskList = ({
+  tasks,
+  loading,
+  error,
+  handleEditSubmitter,
+  editedText,
+  setEditedText,
+}) => {
   return (
     <div className="flex flex-col bg-gray-900 gap-2 container mx-auto p-10">
+      {loading ? (
+        <p className="text-center">{error ? error : "loading..."}</p>
+      ) : (
+        tasks.length === 0 && <p className="text-center">No task to show</p>
+      )}
       {tasks.map((task) => (
-        <TaskItem task={task} key={task.id} />
+        <TaskItem
+          task={task}
+          key={task.id}
+          handleEditSubmitter={handleEditSubmitter}
+          editedText={editedText}
+          setEditedText={setEditedText}
+        />
       ))}
     </div>
   );
